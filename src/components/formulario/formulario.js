2c1688037888edc59e4197cd260a30591479d91d
juan Ramirez <ciscojuan@gmail.com>
2023-04-11 17:57:14 -0500

    addig form components

--- src/components/formulario/formulario.js
+++ src/components/formulario/formulario.js
@@ -1,13 +1,21 @@
 import './formulario.css'
-import TextInput from '../formulario__textInput/index'
+import TextInput from './formulario__textInput'
+import Select from './formulario__select'
+import Boton from './formulario__boton'
 
 const Formulario = () => {
+    const onSubmit = (e) =>{
+        e.preventDefault()
+        console.log('mensaje enviado')
+    }
     return<section className='formulario'>
-        <form>
-        <h2>Rellena el formulario para crear el colaborador.</h2>
-        <TextInput />
-        <TextInput />
-        <TextInput />
+        <form onSubmit={onSubmit}>
+        <h2>Rellena el formulario para crear el colaborador</h2>
+        <TextInput label="Nombre" type="text"  placeholder="Enter you Name"/>
+        <TextInput label="Puesto" type="text" placeholder="Enter your ocupation"/>
+        <TextInput label="Foto" type="text" placeholder="Entre your github profile photo"/>
+        <Select />
+        <Boton/>
         </form>
     </section>
 }
